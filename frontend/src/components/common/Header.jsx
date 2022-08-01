a67c3db009595c42544fd01446ed77e9579fdc45
Yusuke-Suzuki-11 <mr.suzuki.11@gmail.com>
2022-08-01 21:09:43 +0900

    微調整

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -3,12 +3,17 @@ import React from 'react';
 export const Header = () => {
     return (
         <>
-            <div className="w-full bg-purple-900 h-11">
-                <div className='py-1.5 w-5/12 h-full block m-auto'>
-                    <div className=' bg-neutral-50  h-full rounded-md'>
+            <div className="w-full bg-purple-900 h-11 relative">
+                <div className='py-1.5 w-5/12 h-full block m-auto '>
+                    <div className=' bg-neutral-50  h-full rounded-md  '>
                         <p className=' flex justify-center items-center h-full text-sm '>
                             xxxxx内を検索する
                         </p>
+                    </div>
+                </div>
+                <div className="absolute h-full py-1 top-1/2 transform  -translate-y-1/2 right-4">
+                    <div className='bg-green-300 rounded-md h-full aspect-square  '>
+                        A
                     </div>
                 </div>
             </div>
--- frontend/src/components/common/SideBar.jsx
+++ frontend/src/components/common/SideBar.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 export const SideBar = () => {
     return (
         <>
-            <div className=" fixed  w-56   h-full bg-slate-400 ">
+            <div className="fixed w-72 h-full bg-slate-400 ">
                 <div className="flex h-full">
-                    <div className=" w-3/12 bg-slate-800 h-full block">
+                    <div className="w-16  bg-slate-800 h-full block">
                         <div className=" w-full flex justify-center ">
-                            <div className="px-2 w-full ">
-                                <div className=" my-3   aspect-square w-full  bg-white  rounded-md font">
+                            <div className="px-4 w-full ">
+                                <div className=" my-3 aspect-square w-full  bg-white  rounded-md font">
                                     A
                                 </div>
-                                <div className=" my-3   aspect-square w-full  bg-white  rounded-md font">
+                                <div className=" my-3 aspect-square w-full  bg-white  rounded-md font">
                                     A
                                 </div>
                             </div>
