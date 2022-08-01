ced91d43972bd580acaa48a20c08c9f5dfec6615
Yusuke-Suzuki-11 <mr.suzuki.11@gmail.com>
2022-08-01 23:11:53 +0900

    デザイン修正

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Header = () => {
     return (
         <>
-            <div className="w-full bg-purple-900 h-11 relative">
+            <div className="w-full bg-purple-900 h-11 relative" style={{ borderBottom: "1px solid  #c2c2c2ad" }}>
                 <div className='py-1.5 w-5/12 h-full block m-auto '>
                     <div className=' bg-neutral-50  h-full rounded-md  '>
                         <p className=' flex justify-center items-center h-full text-sm '>
--- frontend/src/components/common/SideBar.jsx
+++ frontend/src/components/common/SideBar.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 export const SideBar = () => {
     return (
         <>
-            <div className="fixed w-72 h-full bg-slate-400 ">
-                <div className="flex h-full">
-                    <div className="w-16  bg-slate-800 h-full block">
+            <div className="fixed w-72 h-full bg-purple-900">
+                <div className="flex h-full border-r-2" style={{ borderRight: "1px solid  #c2c2c2ad" }}>
+                    {/*// !左側（組織） */}
+                    <div className="w-16 h-full block shrink-0" style={{ borderRight: "1px solid  #c2c2c2ad" }}>
                         <div className=" w-full flex justify-center ">
                             <div className="px-4 w-full  ">
                                 <div className=" my-2 aspect-square w-full bg-white  rounded-md ">
@@ -15,15 +16,22 @@ export const SideBar = () => {
 
                         </div>
                     </div>
-
-                    <div className='w-full h-full'>
-                        <div className="w-full">
-                            <p className=" font-bold text-lg   ">01Booster</p>
+                    
+                    {/*// !右側(メンバー) */}
+                    <div className='w-full h-full text-white'>
+                        {/*// !組織名 */}
+                        <div className="flex h-12    w-full justify-center items-center px-2" style={{ borderBottom: "1px solid  #c2c2c2ad" }}>
+                            <div className="w-full px-3 py-3 ">
+                                <p className=" font-bold text-lg   ">01Booster</p>
+                            </div>
+                            <div className=" py-1 h-full">
+                                <div className=' h-full aspect-square bg-white rounded-full'>
+                                </div>
+                            </div>
                         </div>
-                        <div className='rounded-full h-full aspect-auto'>
+                        <div className=" ">
                             
                         </div>
-                        
                     </div>
                 </div>
             </div>
