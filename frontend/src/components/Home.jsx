82b4f2ed576e997793f55e23326dff958fc20692
Yusuke-Suzuki-11 <mr.suzuki.11@gmail.com>
2022-08-01 20:56:12 +0900

    微調整

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -6,7 +6,6 @@ export const Home = () => {
         <>
             <Header />
             <div className=" flex">
-                
                 <SideBar />
             </div>
             
--- frontend/src/components/common/SideBar.jsx
+++ frontend/src/components/common/SideBar.jsx
@@ -4,7 +4,24 @@ export const SideBar = () => {
     return (
         <>
             <div className=" fixed  w-56   h-full bg-slate-400 ">
-                
+                <div className="flex h-full">
+                    <div className=" w-3/12 bg-slate-800 h-full block">
+                        <div className=" w-full flex justify-center ">
+                            <div className="px-2 w-full ">
+                                <div className=" my-3   aspect-square w-full  bg-white  rounded-md font">
+                                    A
+                                </div>
+                                <div className=" my-3   aspect-square w-full  bg-white  rounded-md font">
+                                    A
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div>
+                    </div>
+                </div>
             </div>
         </>
     );
