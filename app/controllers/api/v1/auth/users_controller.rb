e943ccf694df1316ded7a04c9a8cf3d95307c1af
Yusuke-Suzuki-11 <mr.suzuki.11@gmail.com>
2022-08-04 19:44:42 +0900

    api処理修正

--- app/controllers/api/v1/auth/users_controller.rb
+++ app/controllers/api/v1/auth/users_controller.rb
@@ -1,24 +1,26 @@
 class Api::V1::Auth::UsersController < ApplicationController
     def invite
-        current_user_const = User.find_by(id: 1)
+        # current_user_const = User.find_by(id: 1)
         
-        post_email = params[:params][:invite_email]
-        targetUser = User.find_by(email: post_email)
-        if targetUser.nil? 
-            render json: {success: false, message: 'ユーザーが見つかりませんでした。'}
-            return
-        end
+        # post_email = params[:params][:invite_email]
+        # targetUser = User.find_by(email: post_email)
+        # if targetUser.nil? 
+        #     render json: {success: false, message: 'ユーザーが見つかりませんでした。'}
+        #     return
+        # end
         
-        if current_user_const.id == targetUser.id
-            return
-        end
+        # if current_user_const.id == targetUser.id
+        #     return
+        # end
         
-        # reaction = Reaction.find_or_initialize_by(to_user_id: params[:user_id], from_user_id: current_user.id)
-        # reaction.update_attributes(
-        #     status: 1
-        # )
+        # reaction = Reaction.find_or_initialize_by(to_user_id: targetUser.id, from_user_id: current_user_const.id)
+        # reaction.status = 0
+        # reaction.save
         
-        
+        render json: {success: true, message: current_user}
+    end
+    
+    def test
         
         render json: {success: true, message: current_user}
     end
--- frontend/src/apis/users.js
+++ frontend/src/apis/users.js
@@ -11,14 +11,12 @@ export const userInvite = (params) => {
     
     return client.post(
         'api/v1/auth/account/invite',
-        {
-            headers: {
-                "access-token": Cookies.get("_access_token"),
-                client: Cookies.get("_client"),
-                uid: Cookies.get("_uid"),
-            },
-            params,
-        })
+        params,
+        {headers: {
+            "access-token": Cookies.get("_access_token"),
+            client: Cookies.get("_client"),
+            uid: Cookies.get("_uid"),
+        }})
         .then(response => {
             console.log(response);
             return response.data;
