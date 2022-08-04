4f0a97484dc9d484c5884905ec5b4f5dac03df9e
Yusuke-Suzuki-11 <mr.suzuki.11@gmail.com>
2022-08-04 19:46:33 +0900

    つながった

--- app/controllers/api/v1/auth/users_controller.rb
+++ app/controllers/api/v1/auth/users_controller.rb
@@ -1,23 +1,28 @@
 class Api::V1::Auth::UsersController < ApplicationController
     def invite
-        # current_user_const = User.find_by(id: 1)
         
-        # post_email = params[:params][:invite_email]
-        # targetUser = User.find_by(email: post_email)
-        # if targetUser.nil? 
-        #     render json: {success: false, message: 'ユーザーが見つかりませんでした。'}
-        #     return
-        # end
+        if current_user.nil?
+            render json: {success: false, message: 'ユーザーが見つかりませんでした。'}
+            return
+        end
         
-        # if current_user_const.id == targetUser.id
-        #     return
-        # end
+        post_email = params[:params][:invite_email]
+        target_user = User.find_by(email: post_email)
+        if target_user.nil? 
+            render json: {success: false, message: 'ユーザーが見つかりませんでした。'}
+            return
+        end
         
-        # reaction = Reaction.find_or_initialize_by(to_user_id: targetUser.id, from_user_id: current_user_const.id)
-        # reaction.status = 0
-        # reaction.save
+        if current_user_const.id == target_user.id
+            render json: {success: false, message: 'ログインユーザーです。'}
+            return
+        end
         
-        render json: {success: true, message: current_user}
+        reaction = Reaction.find_or_initialize_by(to_user_id: target_user.id, from_user_id: current_user_const.id)
+        reaction.status = 0
+        reaction.save
+        
+        render json: {success: true, message: target_user}
     end
     
     def test
