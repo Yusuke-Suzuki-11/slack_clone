e748e6e10338c19bb73cd55cbcacc1f4623ff491
Yusuke-Suzuki-11 <mr.suzuki.11@gmail.com>
2022-08-04 16:13:47 +0900

    reactionモデル作成

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,2 +1,7 @@
 class Reaction < ApplicationRecord
+    
+    belongs_to :to_user, class_name: 'User'
+    belongs_to :from_user, class_name: 'User'
+    
+    enum status: { invite: 0, rejection: 1 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
   include DeviseTokenAuth::Concerns::User
+  
+  has_many :reactions
 end
