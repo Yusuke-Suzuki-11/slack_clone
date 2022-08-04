d78376ee010bd2847f487c84f14903929477d64c
Yusuke-Suzuki-11 <mr.suzuki.11@gmail.com>
2022-08-04 16:07:07 +0900

    マイグレーション作成

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,13 @@
 
 ActiveRecord::Schema[7.0].define(version: 2022_08_04_070031) do
   create_table "reactions", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "to_user_id", null: false
+    t.bigint "from_user_id", null: false
+    t.integer "status", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["from_user_id"], name: "index_reactions_on_from_user_id"
+    t.index ["to_user_id"], name: "index_reactions_on_to_user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -41,4 +46,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_04_070031) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "reactions", "users", column: "from_user_id"
+  add_foreign_key "reactions", "users", column: "to_user_id"
 end
