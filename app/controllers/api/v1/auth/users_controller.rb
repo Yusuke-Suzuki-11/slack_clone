991ce77fee9894fa11b05f15d80b06519925d72a
Yusuke-Suzuki-11 <mr.suzuki.11@gmail.com>
2022-08-04 16:00:47 +0900

    微調整

--- app/controllers/api/v1/auth/users_controller.rb
+++ app/controllers/api/v1/auth/users_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::Auth::UsersController < ApplicationController
     def invite
+        targetUser = User.find_by(email: params['invite_email'])
+        if targetUser.nil? 
+            render json: {success: false, message: 'ユーザーが見つかりませんでした。'}
+            return
+        end
         
-        render json: {success: true, message: params['invite_email']}
+        
+        
+        render json: {success: true, message: targetUser.nil?}
+        # render json: {success: true, message: params['invite_email']}
     end
 end
