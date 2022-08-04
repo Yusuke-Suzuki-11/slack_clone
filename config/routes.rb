a6fbfa6eafb3ad33ddfe683754af352a3de625a0
Yusuke-Suzuki-11 <mr.suzuki.11@gmail.com>
2022-08-04 22:38:38 +0900

    ダイレクトメッセージコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,13 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
     registrations: 'api/v1/auth/registrations'
   }
-  
-
-  
   namespace :api do
     namespace :v1 do
       namespace :auth do
         resources :sessions, only: %i[index]
-        post 'account/invite', to: 'users#invite'
+        post 'account/invite',        to: 'users#invite'
+        get  'direct_message/index',  to: 'direct_messages#index'
+        post 'direct_message/create', to: 'direct_messages#create'
       end
     end
   
