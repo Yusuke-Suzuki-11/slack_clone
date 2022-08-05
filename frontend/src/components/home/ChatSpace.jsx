249b230c17a4b727bed6408b901018e9ad5d0e82
Yusuke-Suzuki-11 <mr.suzuki.11@gmail.com>
2022-08-05 20:23:40 +0900

    インデント修正

--- frontend/src/components/home/ChatSpace.jsx
+++ frontend/src/components/home/ChatSpace.jsx
@@ -1,48 +1,43 @@
-import React from 'react';
+import React from "react";
 
 export const ChatSpace = () => {
-    return (
-        <>
-            <div >
-                <div className="flex">
-                    <div className="w-72 shrink-0">
-                    </div>
-                    <div className='w-full justify-between'>
-                        <div className="fixed h-12 block w-full bg-white" style={{ borderBottom: "1px solid  #c2c2c2ad" }}>
-                            <div className="flex h-12 items-center ml-4">
-                                <p className="font-bold">スレッド</p>
-                            </div>
-                        </div>
-
-                        <div className='h-12'></div>
-                        <div className='fixed w-full h-full bg-green-50'>
-                            {/* コメントスペース */}
-                            <div className=" bg-red-300 w-full h-full flex flex-col justify-end overflow-scroll  ">
-                                {/* //TODOコメントをループさせる */}
-                                <p>
-                                    fdsafa
-                                </p>
-                                <p>
-                                    fdsafa
-                                </p>
-                                <p>
-                                    fdsafa
-                                </p>
-                                
-                                <div className="bg-white mb-20 ">
-                                    <div className="px-4 w-full">
-                                        <textarea className=" w-full h-44 rounded-lg mb-7  " style={{ border: "1px solid  #c2c2c2ad" }}>
-                                        </textarea>
-                                    </div>
-                                </div>
-
-                            </div>
-
-                        </div>
-
-                    </div>
-                </div>
+  return (
+    <>
+      <div>
+        <div className="flex">
+          <div className="w-72 shrink-0"></div>
+          <div className="w-full justify-between">
+            <div
+              className="fixed h-12 block w-full bg-white"
+              style={{ borderBottom: "1px solid  #c2c2c2ad" }}
+            >
+              <div className="flex h-12 items-center ml-4">
+                <p className="font-bold">スレッド</p>
+              </div>
             </div>
-        </>
-    );
-}
+            <div className="h-12"></div>
+            <div className="fixed w-full h-full bg-green-50">
+              {/* コメントスペース */}
+              <div className=" bg-red-300 w-full h-full flex flex-col justify-end overflow-scroll  ">
+                {/* //TODOコメントをループさせる */}
+                <div>
+                  <p>fdsafa</p>
+                  <p>fdsafa</p>
+                  <p>fdsafa</p>
+                </div>
+                <div className="bg-white mb-20 ">
+                  <div className="px-4 w-full">
+                    <textarea
+                      className=" w-full h-44 rounded-lg mb-7  "
+                      style={{ border: "1px solid  #c2c2c2ad" }}
+                    ></textarea>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
