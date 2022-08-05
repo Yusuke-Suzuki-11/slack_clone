6b7200cfa7cc8c3c14bdccaa96166f1aa400e8b3
Yusuke-Suzuki-11 <mr.suzuki.11@gmail.com>
2022-08-05 20:55:30 +0900

    provider取り入れた

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,22 +1,23 @@
-import { Header } from "./common/Header"
-import { SideBar } from "./common/SideBar"
-import { ChatSpace } from "./home/ChatSpace"
-import { useState } from "react"
+import { Header } from "./common/Header";
+import { SideBar } from "./common/SideBar";
+import { ChatSpace } from "./home/ChatSpace";
+import { createContext, useState } from "react";
 
-
+export const HomeContext = createContext();
 export const Home = () => {
-    const [directMessageObjectsArray, setDirectMessageObjectsArray] = useState([]);
-    const [directMessageToUser, setDirectMessageToUser] = useState();
-    
-    return (
-        <>
-            <div className="h-full">
-                <Header />
-                <SideBar />
-
-                <ChatSpace />
-
-            </div>
-        </>
-    )
-}
+  const [directMessageObjectsArray, setDirectMessageObjectsArray] = useState(
+    []
+  );
+  const [directMessageToUser, setDirectMessageToUser] = useState();
+  return (
+    <>
+      <div className="h-full">
+        <Header />
+        <HomeContext.Provider value={{}}>
+          <SideBar />
+          <ChatSpace />
+        </HomeContext.Provider>
+      </div>
+    </>
+  );
+};
