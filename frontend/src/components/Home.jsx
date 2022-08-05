db23ae33bb421bd495e7dc93c398a3ac22b1a4ab
Yusuke-Suzuki-11 <mr.suzuki.11@gmail.com>
2022-08-05 20:34:11 +0900

    微調整

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,12 @@
 import { Header } from "./common/Header"
 import { SideBar } from "./common/SideBar"
 import { ChatSpace } from "./home/ChatSpace"
+import { useState } from "react"
+
 
 export const Home = () => {
+    const [directMessageObjectsArray, setDirectMessageObjectsArray] = useState([]);
+    
     return (
         <>
             <div className="h-full">
--- frontend/src/components/home/ChatSpace.jsx
+++ frontend/src/components/home/ChatSpace.jsx
@@ -6,29 +6,29 @@ export const ChatSpace = () => {
       <div>
         <div className="flex">
           <div className="w-72 shrink-0"></div>
-          <div className="w-full justify-between">
+          <div className="justify-between w-full">
             <div
-              className="fixed h-12 block w-full bg-white"
+              className="fixed block w-full h-12 bg-white"
               style={{ borderBottom: "1px solid  #c2c2c2ad" }}
             >
-              <div className="flex h-12 items-center ml-4">
+              <div className="flex items-center h-12 ml-4">
                 <p className="font-bold">スレッド</p>
               </div>
             </div>
             <div className="h-12"></div>
             <div className="fixed w-full h-full bg-green-50">
               {/* コメントスペース */}
-              <div className=" bg-red-300 w-full h-full flex flex-col justify-end overflow-scroll  ">
+              <div className="flex flex-col justify-end w-full h-full overflow-scroll ">
                 {/* //TODOコメントをループさせる */}
-                <div>
+                <div className="px-4">
                   <p>fdsafa</p>
                   <p>fdsafa</p>
                   <p>fdsafa</p>
                 </div>
-                <div className="bg-white mb-20 ">
-                  <div className="px-4 w-full">
+                <div className="mb-20 bg-white ">
+                  <div className="w-full px-4">
                     <textarea
-                      className=" w-full h-44 rounded-lg mb-7  "
+                      className="w-full rounded-lg h-44 mb-7"
                       style={{ border: "1px solid  #c2c2c2ad" }}
                     ></textarea>
                   </div>
